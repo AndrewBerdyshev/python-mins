c14f27d6f19cc94d3e271480245bd55b079edca0
AndrewBerdyshev <an.berdyshev@gmail.com>
2024-11-19 13:58:44 +0000

    changes

--- min6/solution.py
+++ min6/solution.py
@@ -1,8 +1,11 @@
 from typing import Callable
 import math
+import pytest
 
 
 def flatten(lst: list, depth = math.inf) -> list:
+    if depth < 0:
+        raise NameError('Negative depth')
     result = []
     for el in lst:
         if isinstance(el, list) and depth > 0:
@@ -25,6 +28,8 @@ def runTests():
     assert flatten([[[1]], [[2]], [[3]]], 2) == [1, 2, 3]
     assert flatten([1, [2, [3, [4, [5]]]]], 2) == [1, 2, 3, [4, [5]]]
     assert flatten([1, [2, [3, [4, [5]]]]], 4) == [1, 2, 3, 4, 5]
+    with pytest.raises(NameError):
+        assert flatten([1], -1)
 
 
 runTests()

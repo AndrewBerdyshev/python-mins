962c81e4a9e3afbd21ac79faa6f5010089c849bd
AndrewBerdyshev <an.berdyshev@gmail.com>
2024-11-22 10:01:58 +0000

    changes

--- min8/solution.py
+++ min8/solution.py
@@ -1,11 +1,23 @@
-import pytest
-from io import StringIO
-import sys
+import math
+
+#min6
+def flatten(lst: list, depth = math.inf) -> list:
+    if depth < 0:
+        raise NameError('Negative depth')
+    result = []
+    for el in lst:
+        if isinstance(el, list) and depth > 0:
+            for fel in flatten(el, depth - 1):
+                result.append(fel)
+        else:
+            result.append(el)
+    return result
 
 def format_table(benchmarks, algos, results):
-    max_benchmark_width = max(len(b) for b in benchmarks)
-    max_algo_width = max(len(a) for a in algos)
-    max_width = max(max_benchmark_width, max_algo_width)
+    max_benchmark_width = max(len(x) for x in benchmarks)
+    max_algo_width = max(len(x) for x in algos)
+    max_results_width = max(len(str(x))+3 for x in flatten(results)) # 3 - '.00'
+    max_width = max(max_benchmark_width, max_algo_width, max_results_width)
     
     header = f"| {'Benchmark':<{max_width}} | " + \
              " | ".join(f"{algo:<{max_width}}" for algo in algos) + " |"
@@ -62,10 +74,18 @@ def test4(capsys):
              [[11212121212.23, 112121212.56, 212121212.0], [312121212.3, 212121212.9, 31212121212.9]])
     
     captured = capsys.readouterr()
-    assert '''| Benchmark      | quick sort     | merge sort     | bubble sort    |
----------------------------------------------------------------------
-| best case      | 11212121212.23 | 112121212.56   | 212121212.00   |
-| the worst case | 312121212.30   | 212121212.90   | 31212121212.90 |''' in captured.out
+    assert '''| Benchmark         | quick sort        | merge sort        | bubble sort       |
+---------------------------------------------------------------------------------
+| best case         | 11212121212.23    | 112121212.56      | 212121212.00      |
+| the worst case    | 312121212.30      | 212121212.90      | 31212121212.90    |''' in captured.out
 
-if __name__ == "__main__":
-    pytest.main()
+def test5(capsys):
+    format_table(["best case", "the worst case"],
+             ["quick sort", "merge sort", "bubble sort"],
+             [[10**16, 10**16, 10**16], [10**16, 10**16, 10**16]])
+    
+    captured = capsys.readouterr()
+    assert '''| Benchmark            | quick sort           | merge sort           | bubble sort          |
+---------------------------------------------------------------------------------------------
+| best case            | 10000000000000000.00 | 10000000000000000.00 | 10000000000000000.00 |
+| the worst case       | 10000000000000000.00 | 10000000000000000.00 | 10000000000000000.00 |''' in captured.out
